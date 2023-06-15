ef6b385552c458847d5b18ee27c2b8068982da3a
kuarm <135824468+kuarm@users.noreply.github.com>
2023-06-15 16:26:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,220 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+# import statements
 import streamlit as st
+import numpy as np
+import pandas as pd
+import altair as alt
+import matplotlib.pyplot as plt
+from PIL import Image
+from datetime import datetime
+import time
+from datetime import date
+from io import StringIO
 
-"""
-# Welcome to Streamlit!
+# adding title and text in the app
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.write("Welcome to your first streamli application")
+st.sidebar.title("Share Price analysis for May 2019 to May 2020:")
+st.markdown("This application is a Share Price dashboard for Top 5 Gainers and Losers:")
+st.sidebar.markdown("This application is a Share Price dashboard for Top 5 Gainers and Losers:")
+st.sidebar.title("Gainers")
+select = st.sidebar.selectbox('Share', ['Adani Green Energy', 'GMM Pfaudler', 'AGC Networks', 'Alkyl Amines Chem', 'IOL Chem & Pharma'], key='1')
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+df = pd.DataFrame({'col1': [1,2,3]})
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+x = 10
+'x', x  # <-- Draw the string 'x' and then the value of x
+st.markdown('Streamlit is **_really_ cool**.')
+st.latex(r'''
+   a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
+
+st.write(1234)
+st.write(df)
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+
+st.write(c)
+
+st.title('This is a title')
+st.header('This is a header')
+st.subheader('This is a subheader')
+
+code = '''def hello():
+     print("Hello, Streamlit!")'''
+st.code(code, language='python')
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+df = pd.DataFrame(
+    np.random.randn(50, 20),
+    columns=('col %d' % i for i in range(20)))
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+st.dataframe(df)  
 
-    points_per_turn = total_points / num_turns
+st.dataframe(df, 200, 100)
+st.dataframe(df.style.highlight_max(axis=0))
+st.table(df)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+st.json({
+     'foo': 'bar',
+     'baz': 'boz',
+     'stuff': [
+         'stuff 1',
+         'stuff 2',
+         'stuff 3',
+         'stuff 5',
+     ],
+ })
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+st.area_chart(chart_data)
+st.bar_chart(chart_data)
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
+
+dfmap = pd.DataFrame(
+np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+     columns=['lat', 'lon'])
+
+st.map(dfmap)
+
+image = Image.open('food.png')
+st.image(image, caption='Sunrise by the mountains')
+
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
+
+agree = st.checkbox('I agree')
+if agree:
+    st.write('Great!')
+
+
+genre = st.radio(
+     "What's your favorite movie genre",
+     ('Comedy', 'Drama', 'Documentary'))
+
+if genre == 'Comedy':
+    st.write('You selected comedy.')
+else:
+    st.write("You didn't select comedy.")
+
+
+option = st.selectbox(
+     'How would you like to be contacted?',
+     ('Email', 'Home phone', 'Mobile phone'))
+
+st.write('You selected:', option)
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+
+st.write('You selected:', options)
+
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
+
+values = st.slider(
+     'Select a range of values',
+     0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
+
+start_time = st.slider(
+     "When do you start?",
+     value=datetime(2020, 1, 1, 9, 30),
+     format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
+
+color = st.select_slider(
+     'Select a color of the rainbow',
+     options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+st.write('My favorite color is', color)
+
+
+number = st.number_input('Insert a number')
+st.write('The current number is ', number)
+
+def run_sentiment_analysis(txt):
+    return "Hello"
+
+txt = st.text_area('Text to analyze', '''
+     It was the best of times, it was the worst of times, it was
+     the age of wisdom, it was the age of foolishness, it was
+     the epoch of belief, it was the epoch of incredulity, it
+     was the season of Light, it was the season of Darkness, it
+     was the spring of hope, it was the winter of despair     ''')
+st.write('Sentiment:', run_sentiment_analysis(txt))
+
+d = st.date_input(
+     "When's your birthday",
+     date(2019, 7, 6))
+st.write('Your birthday is:', d)
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+
+
+
+name = st.text_input('Name')
+if not name:
+   st.warning('Please input a name.')
+#    st.stop()
+st.success('Thank you for inputting a name.')
+
+
+color = st.color_picker('Pick A Color', '#00f900')
+st.write('The current color is', color)
+
+form = st.form("my_form")
+form.slider("Inside the form")
+st.slider("Outside the form")
+
+# Now add a submit button to the form:
+form.form_submit_button("Submit")
+
+col1, col2, col3 = st.beta_columns(3)
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
